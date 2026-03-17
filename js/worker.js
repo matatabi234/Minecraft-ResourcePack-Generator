cd1f0dca1dc6bb526f47d736029222d767ad23b5
matatabi <dosken234@gmail.com>
2026-03-17 21:45:49 +0900

    sitemap削除

--- js/worker.js
+++ js/worker.js
@@ -1,5 +1,10 @@
-if (url.pathname === "/sitemap.xml") {
-  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+
+    // 👇 ここが追加部分
+    if (url.pathname === "/sitemap.xml") {
+      return new Response(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
 <url>
@@ -17,8 +22,13 @@ if (url.pathname === "/sitemap.xml") {
 </url>
 
 </urlset>`, {
-    headers: {
-      "Content-Type": "application/xml"
+        headers: {
+          "Content-Type": "application/xml; charset=UTF-8"
+        }
+      });
     }
-  });
-}
+
+    // 👇 それ以外は普通に表示
+    return fetch(request);
+  }
+};
